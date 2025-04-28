0d345fd84d44d96f906040abee51c1100ab525aa
Popnish <afa86@case.edu>
2025-04-28 15:48:41 -0400

    Add initial website files for coffee shop (index, contact, cart, styles, and scripts)

---
+++ js/custom.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // --- Contact Form ---
+  let contactForm = document.querySelector("#contact-form");
+  contactForm.addEventListener("submit", onContactFormSubmit);
+
+  function onContactFormSubmit(event) {
+    event.preventDefault();
+
+    // Getting form data
+    const data = new FormData(event.target);
+    const dataObject = Object.fromEntries(data.entries());
+    console.log(dataObject);
+
+    //Selecting name and crafting thank you message
+    const name = dataObject.name || "User";
+    const message = `Thank you, ${name}. We have received your inquiry.`;
+
+    // Displaying thank you message
+    const thankYou = document.createElement("p");
+    thankYou.textContent = message;
+    document.querySelector("main").appendChild(thankYou);
+
+    // Resetting and removing the form
+    contactForm.reset();
+    contactForm.remove();
+  }
+
+  // --- Login Form ---
+  const loginForm = document.getElementById("login-form");
+  loginForm.addEventListener("submit", onLoginFormSubmit);
+
+  function onLoginFormSubmit(event) {
+    event.preventDefault();
+
+    // Getting form data
+    const data = new FormData(event.target);
+    const dataObject = Object.fromEntries(data.entries());
+    console.log(dataObject);
+
+    const name = dataObject.name || "User";
+    const message = `Welcome, ${name}! You are logged in!`;
+
+    // Displaying welcome message
+    const welcomeMessage = document.createElement("p");
+    welcomeMessage.textContent = message;
+    document.querySelector("main").appendChild(welcomeMessage);
+
+    // Resetting and removing the form
+    loginForm.reset();
+    loginForm.remove();
+  }
+});
